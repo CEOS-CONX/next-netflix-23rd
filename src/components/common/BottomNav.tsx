ec6583ea6055121783c903091dcaa5ea3af5710e
yujinnie <yujinnieohh@gmail.com>
2026-04-30 23:27:02 +0900

    feature: TopNav, BottomNav 구현

--- src/components/common/BottomNav.tsx
+++ src/components/common/BottomNav.tsx
@@ -1,23 +1,45 @@
+'use client';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 const NAV_ITEMS = [
-  { name: 'Home', icon: 'ic-home.svg' },
-  { name: 'Search', icon: 'ic-search.svg' },
-  { name: 'Coming Soon', icon: 'ic-comingsoon.svg' },
-  { name: 'Downloads', icon: 'ic-downloads.svg' },
-  { name: 'More', icon: 'ic-more.svg' },
+  { name: 'Home', icon: 'ic-home.svg', href: '/' },
+  { name: 'Search', icon: 'ic-search.svg', href: '/search' },
+  { name: 'Coming Soon', icon: 'ic-comingsoon.svg', href: '/coming-soon' },
+  { name: 'Downloads', icon: 'ic-downloads.svg', href: '/downloads' },
+  { name: 'More', icon: 'ic-more.svg', href: '/more' },
 ];
 
 export default function BottomNav() {
+  const pathname = usePathname();
+
   return (
-    <nav className="fixed bottom-0 z-50 w-full border-t border-gray-800 bg-black">
-      <div className="flex items-center justify-around py-2">
-        {NAV_ITEMS.map((item) => (
-          <div key={item.name} className="flex cursor-pointer flex-col items-center gap-1">
-            <Image src={`/assets/icons/${item.icon}`} alt={item.name} width={24} height={24} />
-            <span className="text-[10px] text-gray-400">{item.name}</span>
-          </div>
-        ))}
+    <nav className="bg-grey-900 fixed right-0 bottom-0 left-0 z-50 w-full">
+      <div className="flex items-center justify-around py-5">
+        {NAV_ITEMS.map((item) => {
+          const isActive = pathname === item.href;
+
+          return (
+            <Link
+              href={item.href}
+              key={item.name}
+              className="flex cursor-pointer flex-col items-center gap-4"
+            >
+              <div className={`relative h-6 w-6 ${isActive ? '' : 'opacity-40'}`}>
+                <Image
+                  src={`/assets/icons/${item.icon}`}
+                  alt={item.name}
+                  fill
+                  className={isActive ? '' : 'brightness-[0.55]'}
+                />
+              </div>
+              <span className={`text-caption2 ${isActive ? 'text-white' : 'text-grey-700'}`}>
+                {item.name}
+              </span>
+            </Link>
+          );
+        })}
       </div>
     </nav>
   );
