7ed5eadb71fc8890569214e149e377cdf7b4e482
yujinnie <yujinnieohh@gmail.com>
2026-05-01 02:09:19 +0900

    feature: Previews 컴포넌트 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Featured from '@/components/main/Featured';
+import Previews from '@/components/main/Previews';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Featured />
+      <Previews />
     </div>
   );
 }
